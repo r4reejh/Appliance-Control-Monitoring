8a656e3e4f5649f133b0d5a5ccf1240cf47f581a
Reejh Ghosh <r4reejh@hotmail.com>
2017-11-05 20:27:11 +0530

    finished

--- router.js
+++ router.js
@@ -5,5 +5,6 @@ const worker = require('./worker');
 router.get('/rooms',worker.rooms);
 router.get('/consumption',worker.consumption);
 router.post('/toggle',worker.toggle);
+router.post('/init',worker.init);
 
 module.exports = router;
--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 const {spawn} = require('child_process');
 const {datastore} = require('nedb-promise');
+const DateDiff = require('date-diff');
 
 const ard_1 = spawn("python",['/home/pi/Documents/CSI_final/pro_one.py']);
 const ard_2 = spawn("python",['/home/pi/Documents/CSI_final/pro_two.py']);
@@ -31,7 +32,8 @@ async function rooms(req,res,next) {
 }
 
 async function consumption(req,res,next){
-    let switches =await switch_act.find();
+    console.log('log');
+    let switches =await switch_act.find({});
     let ib = [];
     let room1 = {switches:[]};
     let room2 = {switches:[]};
@@ -43,24 +45,27 @@ async function consumption(req,res,next){
         }
         else{
            var ob={};
-           ob[i.switch]=i.status;
+           ob[i.switch]=i.total_consumption;
            room2.switches.push(ob);
         }
     }
+    console.log('log');
     res.send([room1,room2]);
 }
 
 async function toggle(req,res,next){
-    let room = req.body.room;
+    let room = parseInt(String(req.body.room));
     let switcha = parseInt(String(req.body.switch));
     let val = -1;
+    let status ="";
     if(room==1){
+        let doc = await switch_act.findOne({"room":room,"switch":switcha});
         console.log(room1_status[switcha-1])
         if(room1_status[switcha-1]==0){
             val = 1;
-            console.log("walal")
             try{
                 ard_1.stdin.write('1\n');
+                status="active"
             }
             catch(e){
                 console.log(e)
@@ -69,7 +74,14 @@ async function toggle(req,res,next){
         else{
             val = 0;
             try{
+                let now = new Date();
+                let prev = new Date(doc.since);
+                let Dif = new DateDiff(now,prev);
+                let secs = Dif.seconds();
+                let updated_consume = ((secs/3600)*doc.power_rating)+doc.total_consumption;
+                switch_act.update({"room":room,"switch":switcha},{'$set':{"total_consumption":updated_consume}})
                 ard_1.stdin.write('2\n');
+                status="inactive"
             }
             catch(e){
                 console.log(e)
@@ -78,29 +90,40 @@ async function toggle(req,res,next){
         room1_status[switcha-1]=val;
     }
     else{
+        let doc = await switch_act.findOne({"room":room,"switch":switcha});
         if(room2_status[switcha-1]==0){
             val = 1;
             ard_2.stdin.write(String(switcha+4)+"\n");
+            status="active"
         }
         else{
+            let now = new Date();
+            let prev = new Date(doc.since);
+            let Dif = new DateDiff(now,prev);
+            let secs = Dif.seconds();
+            let updated_consume = ((secs/3600)*doc.power_rating)+doc.total_consumption;
+            switch_act.update({"room":room,"switch":switcha},{'$set':{"total_consumption":updated_consume}})
             val = 0;
             ard_2.stdin.write(String(switcha)+"\n");
+            status="inactive"
         }
         room2_status[switcha-1]=val;
     }
+    switch_act.update({"room":room,"switch":switcha},{'$set':{"status":status,"since":new Date().toUTCString()}})
     res.send("done")
 }
 
-async function init(){
+async function init(req,res,next){
     let r = [[1],[1,2,3,4]];
     let index = 1;
     for(i of r){
         console.log(i);
         for(j of i){
-            switch_act.insert({"room":index,"switch":j,"status":"inactive","since":new Date().getUTCDate,"total_uptime":0,"total_consumption":0})
+            switch_act.insert({"room":index,"switch":j,"status":"inactive","since":new Date().toUTCString(),"total_uptime":0,"total_consumption":0,"power_rating":50 })
         }
         index += 1;
     }
+    res.send('done');
 }
 
 async function initdb(){
@@ -122,3 +145,4 @@ initdb();
 exports.rooms = rooms;
 exports.consumption = consumption;
 exports.toggle = toggle;
+exports.init = init;
